4a478a4d9e14293b694b32f3634881256080741d
Hartz-byte <harshmail281199@gmail.com>
2024-04-02 13:46:53 +0530

    update

--- src/Pages/MainPage/MainPage.js
+++ src/Pages/MainPage/MainPage.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./MainPage.css";
+import Drawer from "../Drawer/Drawer";
 
 const MainPage = () => {
   const [colors, setColors] = useState([]);
 
+  // api call for colors data
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -22,29 +24,63 @@ const MainPage = () => {
   }, []);
 
   return (
-    <div>
+    <>
       <div className="mainContainer">
         {/* filter by text */}
-        <h4 className="filter-text">Filter By</h4>
+        <h4 className="filterText">Filter By</h4>
 
-        {/* color */}
-        <div>
-          {/* text */}
-          <h5 className="color-text">color:</h5>
+        {/* color and title / subtitle input */}
+        <div className="flexDisplay">
+          {/* color */}
+          <div>
+            {/* text */}
+            <h5 className="colorText">color:</h5>
 
-          {/* color display */}
-          <div className="colorDisplay">
-            {colors.map((color, index) => (
-              <div
-                key={index}
-                className="colorCircle"
-                style={{ backgroundColor: color }}
+            {/* color display */}
+            <div className="colorDisplay">
+              {colors.map((color, index) => (
+                <div
+                  key={index}
+                  className="colorCircle"
+                  style={{ backgroundColor: color }}
+                />
+              ))}
+            </div>
+          </div>
+
+          {/* title / subtitle input */}
+          <div>
+            {/* text */}
+            <h5 className="colorText">title / subtitle:</h5>
+
+            {/* input */}
+            <div>
+              <input
+                type="text"
+                placeholder="Search across title and subtitle"
+                className="inputField"
               />
-            ))}
+            </div>
           </div>
         </div>
+
+        {/* creatives process bar */}
+        <div className="process">
+          {/* process bar */}
+          <div className="processBar" />
+
+          {/* process text */}
+          <div>
+            <h5 className="processText">0 / 5 Creatives</h5>
+          </div>
+        </div>
+
+        {/* add creative button */}
+        <div>
+          <button className="addButton">+ Add Creative</button>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
